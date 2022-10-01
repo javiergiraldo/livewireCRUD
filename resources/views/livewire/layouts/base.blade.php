55553182e33b1a9814c722fef651665b0124a3e1
Javier Giraldo <pac8314@gmail.com>
2022-10-01 02:00:46 -0500

    Archivos Iniciales

---
+++ resources/views/livewire/layouts/base.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel livewire CRUD</title>
+
+    {{-- Bootstrap Styles --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+        @livewireStyles </head>
+
+<body>
+
+    {{ $slot }}
+
+    {{-- Bootstrap Scripts --}}
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js"></script>
+
+    @livewireScripts
+</body>
+
+</html>
